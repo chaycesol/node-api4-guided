5023c8af32bb772f6db323bf4ce9696047a5c9e4
chaycesol <chayce.solchaga@gmail.com>
2020-09-03 10:43:15 -0700

    nested shouts router

--- api/api-router.js
+++ api/api-router.js
@@ -1,42 +1,16 @@
 const express = require("express");
 
 const Shouts = require("../shouts/shouts-model.js");
-
+const shoutsRouter = require('../shouts/shouts-router');
 const router = express.Router();
 
 router.use(express.json());
-
+router.use('/shouts/', shoutsRouter);
 router.get("/", (req, res) => {
   res.status(200).json({ api: "up" });
 });
 
-router.get("/shouts", (req, res, next) => {
-  Shouts.find()
-    .then(shouts => {
-      res.status(200).json(shouts);
-    })
-    .catch(error => next(error));
-});
 
-router.post("/shouts", (req, res, next) => {
-  Shouts.add(req.body)
-    .then(shout => {
-      res.status(201).json(shout);
-    })
-    .catch(error => next(error));
-});
-
-router.delete("/shouts/:id", (req, res) => {
-  Shouts.remove(req.params.id)
-    .then(count => {
-      if (count) {
-        res.status(204).end();
-      } else {
-        res.status(404).json({ message: "not found" });
-      }
-    })
-    .catch(error => next(error));
-});
 
 router.use(errorHandler);
 
